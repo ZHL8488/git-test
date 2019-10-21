ad4261f180fb855e273ace7fe2b242569456aae8
lzh <lzh@itcast.cn>
2019-10-21 19:49:43 +0800

    修改了MyGit.class文件

--- git-myproject/src/com/itheima/mygit/MyGit.java
+++ git-myproject/src/com/itheima/mygit/MyGit.java
@@ -4,4 +4,7 @@ public class MyGit {
     public static void main(String[] args) {
         System.out.println("xixi");
     }
+    public static void fun(){
+        System.out.println("hello");
+    }
 }
