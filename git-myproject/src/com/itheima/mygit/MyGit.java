e8920cd13627f742b0481c99525aa0254d61575b
lzh <lzh@itcast.cn>
2019-10-21 20:16:52 +0800

    2号修改MyGit文件

--- git-myproject/src/com/itheima/mygit/MyGit.java
+++ git-myproject/src/com/itheima/mygit/MyGit.java
@@ -3,11 +3,14 @@ package com.itheima.mygit;
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("xixi");
+        System.out.println("我添加了一些代码");
     }
     public static void fun(){
         System.out.println("hello");
+        System.out.println("我添加了一些代码");
     }
     public static void fun2(){
         System.out.println("haha");
+        System.out.println("我添加了一些代码");
     }
 }
